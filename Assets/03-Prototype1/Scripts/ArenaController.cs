f35ae00b3d58b17a071af0950caa3c9c7cc46d3f
EricReynolds63 <err63@scarletmail.rutgers.edu>
2023-02-18 15:49:21 -0500

    Ghost Spawning
    Arena Controller now spawns transparent ghosts. Currently they're not rotated properly and don't spawn outside the arena or move

--- Assets/03-Prototype1/Scripts/ArenaController.cs
+++ Assets/03-Prototype1/Scripts/ArenaController.cs
@@ -9,15 +9,18 @@ public class ArenaController : MonoBehaviour
 
 	[Header("Inscribed")]
 	public GameObject pipPrefab;
+	public GameObject ghostPrefab;
 	public float pipDropDelay = 1f;
-	public int arenaX = 30;
-	public int arenaY = 20;
+	public float ghostDelay = 1f;
+	public float arenaX = 30f;
+	public float arenaY = 20f;
 
 	// Start is called before the first frame update
 	void Start()
     {
 		//Start Dropping Pips
-		Invoke("DropPip", 5f);
+		Invoke("DropPip", 3f);
+		Invoke("SpawnGhost", 3f);
 	}
 
     // Update is called once per frame
@@ -30,11 +33,23 @@ public class ArenaController : MonoBehaviour
 	{
 		GameObject pip;
 		Vector3 pos = new Vector3();
-			pos.z = transform.position.z + Random.Range(-10.0f, 10.0f);
-			pos.x = transform.position.x + Random.Range(-15.0f, 15.0f);
+			pos.z = transform.position.z + Random.Range(-(arenaY/2), (arenaY/2));
+			pos.x = transform.position.x + Random.Range(-(arenaX/2), (arenaX/2));
 			pos.y = 10;
 		pip = Instantiate<GameObject>(pipPrefab);
 			pip.transform.position = pos;
 		Invoke("DropPip", pipDropDelay);
+	}
+
+	void SpawnGhost()
+	{
+		GameObject ghost;
+		Vector3 pos = new Vector3();
+		pos.z = transform.position.z + Random.Range(-(arenaY / 2), (arenaY / 2));
+		pos.x = transform.position.x + Random.Range(-(arenaX / 2), (arenaX / 2));
+		pos.y = 1;
+		ghost = Instantiate<GameObject>(ghostPrefab);
+		ghost.transform.position = pos;
+		Invoke("SpawnGhost", ghostDelay);
 	}
 }
